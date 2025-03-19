b76c0e658aad8f07a14cba07f7f7e15559a5404b
eblackburn4 <eblackburn4@sheffield.ac.uk>
2025-03-19 11:07:30 +0000

    initial commit

---
+++ Proposal code.R
@@ -0,0 +1,12 @@
+
+## ---------------------------
+## Purpose of script: code for generation of maps and graphs in diss proposal
+## Author: Ned Blackburn
+## Date Created: 2025-03-19
+
+options(scipen = 6, digits = 5) 
+library(tidyverse)
+library(hrbrthemes)
+
+## ---------------------------
+
